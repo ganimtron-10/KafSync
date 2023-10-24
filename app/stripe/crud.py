4c0df38d7a1af437d302edb2ee1f2b54c1a205c6
Praneeth Shetty <praneethshetty10@gmail.com>
2023-10-24 17:10:48 +0530

    adding remaining crud operations

--- app/stripe/crud.py
+++ app/stripe/crud.py
@@ -13,3 +13,15 @@ stripe.api_key = os.getenv("STRIPE_API_KEY")
 
 def create_stripe_customer(customer: Customer):
     return stripe.Customer.create(name=customer.name, email=customer.email)
+
+
+def get_stripe_customer(customer_id: str):
+    return stripe.Customer.retrieve(id=customer_id)
+
+
+def update_stripe_customer(customer_id: str, customer: Customer):
+    return stripe.Customer.modify(id=customer_id, name=customer.name, email=customer.email)
+
+
+def delete_stripe_customer(customer_id: str):
+    return stripe.Customer.delete(customer_id)
