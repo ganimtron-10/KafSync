e0345104d846be61bcfd389b0f5cdd5461ee43f3
Praneeth Shetty <praneethshetty10@gmail.com>
2023-10-24 22:21:26 +0530

    updating sql crud to integrate kafka producer

--- app/sql/crud.py
+++ app/sql/crud.py
@@ -1,6 +1,9 @@
+import json
+
 from sqlalchemy.orm import Session
 
 from . import models, schemas
+from ..kafka import producer
 
 
 def get_customer(db: Session, customer_id: int):
@@ -14,20 +17,32 @@ def get_customer_by_email(db: Session, email: str):
 def create_customer(db: Session, customer: schemas.Customer):
     db_customer = models.Customer(email=customer.email, name=customer.name)
     db.add(db_customer)
-    db.commit()
-    db.refresh(db_customer)
-    return db_customer
+    print(json.dumps(customer.json()))
+    remaining_message = producer.produce_message(
+        json.dumps(customer.json()), partition=0)
+    if remaining_message == 0:
+        db.commit()
+        db.refresh(db_customer)
+        return db_customer
 
 
 def update_customer(db: Session, customer_id: int, customer: schemas.Customer):
     row_cnt = db.query(models.Customer).filter(models.Customer.id == customer_id).update(
         {models.Customer.name: customer.name, models.Customer.email: customer.email}, synchronize_session=False)
-    db.commit()
-    return row_cnt
+    print(json.dumps(customer.json()))
+    remaining_message = producer.produce_message(
+        json.dumps(customer.json()), partition=1)
+    if remaining_message == 0:
+        db.commit()
+        return row_cnt
 
 
 def delete_customer(db: Session, customer_id: int):
     row_cnt = db.query(models.Customer).filter(models.Customer.id ==
                                                customer_id).delete(synchronize_session=False)
-    db.commit()
-    return row_cnt
+    print(json.dumps(customer_id))
+    remaining_message = producer.produce_message(
+        json.dumps(customer_id), partition=2)
+    if remaining_message == 0:
+        db.commit()
+        return row_cnt
