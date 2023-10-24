502796885062b50f1bedd9bfcc6d37b5f0ec5613
Praneeth Shetty <praneethshetty10@gmail.com>
2023-10-24 18:18:34 +0530

    renaming functions to avoid repetation

--- app/stripe/crud.py
+++ app/stripe/crud.py
@@ -11,28 +11,28 @@ load_dotenv(find_dotenv())
 stripe.api_key = os.getenv("STRIPE_API_KEY")
 
 
-def create_stripe_customer(customer: schemas.Customer):
+def create_customer(customer: schemas.Customer):
     try:
         return stripe.Customer.create(name=customer.name, email=customer.email)
     except:
         return {'error': "Unable to create Customer"}
 
 
-def get_stripe_customer(customer_id: str):
+def get_customer(customer_id: str):
     try:
         return stripe.Customer.retrieve(id=customer_id)
     except:
         return {'error': "Unable to get Customer"}
 
 
-def update_stripe_customer(customer_id: str, customer: schemas.Customer):
+def update_customer(customer_id: str, customer: schemas.Customer):
     try:
         return stripe.Customer.modify(id=customer_id, name=customer.name, email=customer.email)
     except:
         return {'error': "Unable to update Customer"}
 
 
-def delete_stripe_customer(customer_id: str):
+def delete_customer(customer_id: str):
     try:
         return stripe.Customer.delete(customer_id)
     except:
