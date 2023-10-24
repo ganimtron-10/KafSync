467a262cd15592bf8b045bb10b713edbb88d6641
Praneeth Shetty <praneethshetty10@gmail.com>
2023-10-24 23:48:50 +0530

    adding stripe connection to kafka consumer

--- app/kafka/consumer.py
+++ app/kafka/consumer.py
@@ -2,20 +2,28 @@ import json
 
 from confluent_kafka import Consumer, Message
 
+from ..sql import schemas
+from ..stripeapp import crud as stripe_crud
+
 
 def handle_message(msg: Message):
     partition = msg.partition()
     if partition == 0:
         # Create
         data = json.loads(msg.value().decode('utf-8'))
+        customer = schemas.Customer(**data['customer'])
+        stripe_crud.create_customer(customer)
         print(f"[Create] Message - {data['customer']}")
     elif partition == 1:
         # Update
         data = json.loads(msg.value().decode('utf-8'))
+        customer = schemas.Customer(**data['customer'])
+        stripe_crud.update_customer(data['customer_id'], customer)
         print(f"[Update] Message - {data['customer_id']} - {data['customer']}")
     elif partition == 2:
         # Delete
         data = json.loads(msg.value().decode('utf-8'))
+        stripe_crud.delete_customer(data['customer_id'])
         print(f"[Delete] Message - {data['customer_id']}")
     else:
         print("Unable to handle this message")
