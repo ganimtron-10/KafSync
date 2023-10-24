e2367fb459caf3fd3455ab3b37a03dafcc3637f5
Praneeth Shetty <praneethshetty10@gmail.com>
2023-10-25 02:51:20 +0530

    adding create msg parameter to use it without producing message

--- app/sql/crud.py
+++ app/sql/crud.py
@@ -14,49 +14,50 @@ def get_customer_by_email(db: Session, email: str):
     return db.query(models.Customer).filter(models.Customer.email == email).first()
 
 
-def create_customer(db: Session, customer: schemas.Customer):
+def create_customer(db: Session, customer: schemas.Customer, create_message: bool = True):
     db_customer = models.Customer(email=customer.email, name=customer.name)
     db.add(db_customer)
     db.flush()
     db.refresh(db_customer)
-    data = {
-        "customer_id": db_customer.id,
-        "customer": customer.model_dump()
-    }
-    remaining_message = producer.produce_message(
-        json.dumps(data), topic="localtostripe", partition=0)
-    if remaining_message == 0:
-        db.commit()
-        return db_customer
+    if create_message:
+        data = {
+            "customer_id": db_customer.id,
+            "customer": customer.model_dump()
+        }
+        producer.produce_message(
+            json.dumps(data), topic="localtostripe", partition=0)
+    db.commit()
+    return db_customer
 
 
-def update_customer(db: Session, customer_id: int, customer: schemas.Customer):
+def update_customer(db: Session, customer_id: int, customer: schemas.Customer, create_message: bool = True):
     row_cnt = db.query(models.Customer).filter(models.Customer.id == customer_id).update(
         {models.Customer.name: customer.name, models.Customer.email: customer.email}, synchronize_session=False)
     if row_cnt > 0:
-        data = {
-            "customer_id": customer_id,
-            "customer": customer.model_dump()
-        }
-        remaining_message = producer.produce_message(
-            json.dumps(data), topic="localtostripe", partition=1)
-        if remaining_message == 0:
-            db.commit()
-            return row_cnt
+        if create_message:
+            data = {
+                "customer_id": customer_id,
+                "customer": customer.model_dump()
+            }
+            producer.produce_message(
+                json.dumps(data), topic="localtostripe", partition=1)
+        db.commit()
+        return row_cnt
 
 
-def delete_customer(db: Session, customer_id: int):
+def delete_customer(db: Session, customer_id: int, create_message: bool = True):
     row_cnt = db.query(models.Customer).filter(models.Customer.id ==
                                                customer_id).delete(synchronize_session=False)
     if row_cnt > 0:
-        data = {
-            "customer_id": customer_id
-        }
-        remaining_message = producer.produce_message(
-            json.dumps(data), topic="localtostripe", partition=2)
-        if remaining_message == 0:
-            db.commit()
-            return row_cnt
+        if create_message:
+            data = {
+                "customer_id": customer_id
+            }
+            producer.produce_message(
+                json.dumps(data), topic="localtostripe", partition=2)
+
+        db.commit()
+        return row_cnt
 
 
 def create_idmap(db: Session, localid: int, externalid: str):
