464e4e7fdea2854f17f3ee0978104f3daf34fec4
Praneeth Shetty <praneethshetty10@gmail.com>
2023-10-25 01:09:48 +0530

    updating stripe crud operations to return error details

--- app/stripeapp/crud.py
+++ app/stripeapp/crud.py
@@ -14,26 +14,26 @@ stripe.api_key = os.getenv("STRIPE_API_KEY")
 def create_customer(customer: schemas.Customer):
     try:
         return stripe.Customer.create(name=customer.name, email=customer.email)
-    except:
-        return {'error': "Unable to create Customer"}
+    except Exception as e:
+        return {'error': "Unable to create Customer", 'details': e}
 
 
 def get_customer(customer_id: str):
     try:
         return stripe.Customer.retrieve(id=customer_id)
-    except:
-        return {'error': "Unable to get Customer"}
+    except Exception as e:
+        return {'error': "Unable to get Customer", 'details': e}
 
 
 def update_customer(customer_id: str, customer: schemas.Customer):
     try:
         return stripe.Customer.modify(id=customer_id, name=customer.name, email=customer.email)
-    except:
-        return {'error': "Unable to update Customer"}
+    except Exception as e:
+        return {'error': "Unable to update Customer", 'details': e}
 
 
 def delete_customer(customer_id: str):
     try:
         return stripe.Customer.delete(customer_id)
-    except:
-        return {'error': "Unable to delete Customer"}
+    except Exception as e:
+        return {'error': "Unable to delete Customer", 'details': e}
