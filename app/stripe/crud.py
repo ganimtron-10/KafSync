c3b7b007e8bfd21953002ab661cb834802a8892d
Praneeth Shetty <praneethshetty10@gmail.com>
2023-10-24 17:14:37 +0530

    adding exception handling to handle errors

--- app/stripe/crud.py
+++ app/stripe/crud.py
@@ -12,16 +12,28 @@ stripe.api_key = os.getenv("STRIPE_API_KEY")
 
 
 def create_stripe_customer(customer: Customer):
-    return stripe.Customer.create(name=customer.name, email=customer.email)
+    try:
+        return stripe.Customer.create(name=customer.name, email=customer.email)
+    except:
+        return {'error': "Unable to create Customer"}
 
 
 def get_stripe_customer(customer_id: str):
-    return stripe.Customer.retrieve(id=customer_id)
+    try:
+        return stripe.Customer.retrieve(id=customer_id)
+    except:
+        return {'error': "Unable to get Customer"}
 
 
 def update_stripe_customer(customer_id: str, customer: Customer):
-    return stripe.Customer.modify(id=customer_id, name=customer.name, email=customer.email)
+    try:
+        return stripe.Customer.modify(id=customer_id, name=customer.name, email=customer.email)
+    except:
+        return {'error': "Unable to update Customer"}
 
 
 def delete_stripe_customer(customer_id: str):
-    return stripe.Customer.delete(customer_id)
+    try:
+        return stripe.Customer.delete(customer_id)
+    except:
+        return {'error': "Unable to delete Customer"}
