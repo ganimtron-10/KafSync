d8c385cb3fc9305025d683e42112c446567c8b78
Praneeth Shetty <praneethshetty10@gmail.com>
2023-11-14 03:31:52 +0530

    updating stripe crud docs and type hint

--- app/stripeapp/crud.py
+++ app/stripeapp/crud.py
@@ -11,35 +11,79 @@ load_dotenv(find_dotenv())
 stripe.api_key = os.getenv("STRIPE_API_KEY")
 
 
-def create_customer(customer: schemas.Customer):
+def create_customer(customer: schemas.Customer) -> Dict | stripe.Customer:
+    """
+    Create a new customer on the Stripe platform.
+
+    Parameters:
+    - customer (schemas.Customer): Customer data for creation.
+
+    Returns:
+    dict or stripe.Customer: Created customer data or error details.
+    """
+
     try:
         return stripe.Customer.create(name=customer.name, email=customer.email)
     except Exception as e:
         return {'error': "Unable to create Customer", 'details': e}
 
 
-def get_customer(customer_id: str):
+def get_customer(customer_id: str) -> Dict | stripe.Customer:
+    """
+    Retrieve customer data from the Stripe platform.
+
+    Parameters:
+    - customer_id (str): ID of the customer to retrieve.
+
+    Returns:
+    dict or stripe.Customer: Retrieved customer data or error details.
+    """
     try:
         return stripe.Customer.retrieve(id=customer_id)
     except Exception as e:
         return {'error': "Unable to get Customer", 'details': e}
 
 
-def get_customer_list():
+def get_customer_list() -> Dict | List[stripe.Customer]:
+    """
+    Retrieve a list of customers from the Stripe platform.
+
+    Returns:
+    dict or List[stripe.Customer]: List of customer data or error details.
+    """
     try:
         return stripe.Customer.list()
     except Exception as e:
         return {'error': "Unable to get Customer", 'details': e}
 
 
-def update_customer(customer_id: str, customer: schemas.Customer):
+def update_customer(customer_id: str, customer: schemas.Customer) -> Dict | stripe.Customer:
+    """
+    Update an existing customer on the Stripe platform.
+
+    Parameters:
+    - customer_id (str): ID of the customer to update.
+    - customer (schemas.Customer): Updated customer data.
+
+    Returns:
+    dict or stripe.Customer: Updated customer data or error details.
+    """
     try:
         return stripe.Customer.modify(id=customer_id, name=customer.name, email=customer.email)
     except Exception as e:
         return {'error': "Unable to update Customer", 'details': e}
 
 
-def delete_customer(customer_id: str):
+def delete_customer(customer_id: str) -> Dict | stripe.Deleted:
+    """
+    Delete a customer from the Stripe platform.
+
+    Parameters:
+    - customer_id (str): ID of the customer to delete.
+
+    Returns:
+    dict or stripe.Deleted: Deletion confirmation or error details.
+    """
     try:
         return stripe.Customer.delete(customer_id)
     except Exception as e:
