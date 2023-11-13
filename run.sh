91d7e9c022722e9b51b0f4227432c7910ff81865
Praneeth Shetty <praneethshetty10@gmail.com>
2023-11-14 00:20:16 +0530

    updating run script to explain utilizing both approach

--- run.sh
+++ run.sh
@@ -14,11 +14,20 @@ echo 'STRIPE_API_KEY=<Your Stripe API key>' >> .env
 # Step 4: Execute Kafka Resources Setup
 python -m app.kafka.admin.py
 
-# Step 5: Start the Scheduled Poller
+# Step 5: Start the Server
+uvicorn app.main:app --reload &
+
+# Step 6: Choose Between Polling or Webhook Setup
+### Option 1: Polling
+### Start the Scheduled Poller
 python -m app.schedule-poll.py &
 
-# Step 6: Start the Server
-uvicorn app.main:app --reload &
+### Option 2: Webhook Setup
+### Manually set up a webhook endpoint in your local environment. I am using localtunnel for exposing the local server to the internet.
+lt -p 8000
+### Note the generated public URL, then go to your Stripe account and configure the webhook endpoint 
+### Webhook Endpoint URL would look like https://{public_URL}/api/v1/customers/webhook
+
 
 # Step 7: Start the Kafka Consumer
 python -m app.kafka.consumer.py &
