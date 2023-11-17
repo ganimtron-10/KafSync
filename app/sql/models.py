67d7d13e6940ab859d1f1b570560ae5e2876dbb4
Praneeth Shetty <praneethshetty10@gmail.com>
2023-11-17 13:29:25 +0530

    updating data to not be null

--- app/sql/models.py
+++ app/sql/models.py
@@ -6,13 +6,13 @@ from .database import Base
 class Customer(Base):
     __tablename__ = "customers"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    email = Column(String, unique=True)
+    id = Column(Integer, nullable=False, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
 
 
 class IDMap(Base):
     __tablename__ = "idmap"
 
-    localid = Column(Integer, primary_key=True, index=True)
-    externalid = Column(String)
+    localid = Column(Integer, primary_key=True, index=True, nullable=False)
+    externalid = Column(String, nullable=False)
